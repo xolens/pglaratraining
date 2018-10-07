1e6c67a2aad834d6c0e3fb266d6c05b5132664c5
xolens <toube_pierre@yahoo.fr>
2018-10-07 12:44:00 +0100

    Xolens\PgLaratraining\App\Repository\View\StudentHandicapViewRepository StudentHandicap::TRAINER_PROPERTY error fixed

--- src/Xolens/PgLaratraining/App/Repository/View/StudentHandicapViewRepository.php
+++ src/Xolens/PgLaratraining/App/Repository/View/StudentHandicapViewRepository.php
@@ -17,26 +17,26 @@ class StudentHandicapViewRepository extends AbstractReadableRepository implement
 
     public function paginateByStudent($parentId, $perPage=50, $page = null,  $columns = ['*'], $pageName = 'page'){
         $parentFilterer = new Filterer();
-        $parentFilterer->equals(StudentHandicap::TRAINER_PROPERTY, $parentId);
+        $parentFilterer->equals(StudentHandicap::STUDENT_PROPERTY, $parentId);
         return $this->paginateFiltered($parentFilterer, $perPage, $page,  $columns, $pageName);
     }
 
     public function paginateByStudentSorted($parentId, Sorter $sorter, $perPage=50, $page = null,  $columns = ['*'], $pageName = 'page'){
         $parentFilterer = new Filterer();
-        $parentFilterer->equals(StudentHandicap::TRAINER_PROPERTY, $parentId);
+        $parentFilterer->equals(StudentHandicap::STUDENT_PROPERTY, $parentId);
         return $this->paginateSortedFiltered($sorter, $parentFilterer, $perPage, $page,  $columns, $pageName);
     }
     
     public function paginateByStudentFiltered($parentId, Filterer $filterer, $perPage=50, $page = null,  $columns = ['*'], $pageName = 'page'){
         $parentFilterer = new Filterer();
-        $parentFilterer->equals(StudentHandicap::TRAINER_PROPERTY, $parentId);
+        $parentFilterer->equals(StudentHandicap::STUDENT_PROPERTY, $parentId);
         $parentFilterer->and($filterer);
         return $this->paginateFiltered($parentFilterer, $perPage, $page,  $columns, $pageName);
     }
     
     public function paginateByStudentSortedFiltered($parentId, Sorter $sorter, Filterer $filterer, $perPage=50, $page = null,  $columns = ['*'], $pageName = 'page'){
         $parentFilterer = new Filterer();
-        $parentFilterer->equals(StudentHandicap::TRAINER_PROPERTY, $parentId);
+        $parentFilterer->equals(StudentHandicap::STUDENT_PROPERTY, $parentId);
         $parentFilterer->and($filterer);
         return $this->paginateSortedFiltered($sorter, $parentFilterer, $perPage, $page,  $columns, $pageName);
     }
